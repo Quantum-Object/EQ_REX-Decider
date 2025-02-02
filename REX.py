58bb7d76ec37f2626e442c479537226e97800d3f
Your Name <your.email@example.com>
2025-02-02 08:02:29 +0200

    added w to NFA

--- REX.py
+++ REX.py
@@ -8,32 +8,13 @@ class REX:
     def __init__(self, s):
         self.s = s
         
-    def to_NFA(self):
-        # (*,+,|) and (0,1) or (a,b,c)
-        # so now we should look for brackets and strat parsing them in some stack manner
-        # for instance
-        #1. break s into ()()()().... 
-        #2. make NFA for each of these:
-        #3. so now make subNFA OK
-        # a string is 
-        """ 
-        (X)->X
-        X+Y-> Conc(X,Y)
-        ua*b-> Conc(u,conc(a.S(),b))
-        X|Y-> union(X,Y)
-        
-        """
-        
-            
-            
-
-            
+    def to_NFA(s ):
+        # this should only deal with the following form 
+        # {w| w is a binary string not containing {*,+}}
+        w=s
+        N=NFA(len(w)+1,{},[len(w)+1])
+        for i in range(1,N.q):
+            N.f[i]=[(w[i-1],i+1)]
+        return N
         
         
-        
-        # what can we have now with no brackets well:
-        # like (1*0 or 1+1+0 clearly we have to take it in account 1111==1+1+1+1 
-        #now priorty of operation as follows:
-        # star,conc,union (*,+,|)
-        # for r1*r2+r3|r4
-        # this means we should do it recursively starting from | and then + then *
