e2a5a0bb465d88c6ec8db4ffb83a5590c0f1c2d7
Your Name <your.email@example.com>
2025-02-03 11:02:23 +0200

    started sub functions for RPN

--- REX.py
+++ REX.py
@@ -7,7 +7,7 @@ class REX:
     def __init__(self, s):
         self.s = s
         
-    def to_NFA(s ):
+    def w_to_NFA(s ):
         # this should only deal with the following form 
         # {w| w is a binary string not containing {*,+}}
         w=s
@@ -16,4 +16,42 @@ class REX:
             N.f[i]=[(w[i-1],i+1)]
         return N
         
+    def to_NFA(self):
+        #here we turn a full Rex to NFA
+        #for (11)*0 first this is (s1)*s2
+        pass
+    
+    # this adds necessary .<concat> operator
+    def add_conc(w):
+        # first turn every for 1111*00-> 111.1*.00
+        # also
+        # 11100 -> 11100
+        out=[]
+        for i in range(0,len(w)-1):
+            if w[i+1]=='*' and  w[i]!=')':
+                out.append('.')
+                out.append(w[i])
+            else:
+                out.append(w[i])
+            if (w[i]=='*' and w[i+1] not in['|',')','(','.','*']):
+                out.append('.')
+        out.append(w[len(w)-1])
+        return out
+            
+   
+        
+        
+    def RPN(w):
+        #here we turn a string into a list of string representing rex in Postfix notation
+        # for 11|0 -> [11 , 0 , |] 
+        # for s1*|s2 -> [s1,*,s2,|]
+        # for (s1 | s2)* -> [s1 , s2 , | , *]
+        # for (s1 | s2 | s3) -> [s1 , s2 , s3 , | , |]
+        # operators {*,|,.}
+        # ok the big problem comes when w is like 11*0 this 1 , 1 ,* , 0
+        pn=[]
+        st=[]
+        w=(REX.add_conc(w))
+        
+        print(w)
         
