301709526e6a89b8e3eff7a047444b1bed84f945
Your Name <your.email@example.com>
2025-02-05 13:19:56 +0200

    Implement main function to accept user input for regular expressions and display equivalent NFA results

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from REX import REX
 from NFA import NFA
 
-r=REX("ab*")
-print(r.RPN())
 
-N=(r.to_NFA())
-N.show()
-print(N.A_NFA("abbba"))
+
+def main():
+    rex=REX(input("Regular Expression :").replace(" ", ""))
+    
+    N=rex.to_NFA()
+    print("Equavilant NFA :")
+    N.show()
+    for i in range(100):
+        w= bin(i)[2:]
+        s1=w.replace('1', 'a').replace('0', 'b')
+        s2=w.replace('0', 'a').replace('1', 'b')
+        t=N.A_NFA(s1)
+        t2=N.A_NFA(s2)
+        if t:
+            print(f"{s1} ∈ L({rex.s}) ✅")
+        if t2:
+            print(f"{s2} ∈ L({rex.s}) ✅")
+        
+
+#(a|b*)(a|b)
+main()
